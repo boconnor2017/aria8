bb9e7057656c517d4083c200a85a52f219c25dd8
[Brendan OConnor] <[boconnor@vmware.com]>
2024-11-13 16:34:34 -0500

    Init v6

--- hesiod-aria8.py
+++ hesiod-aria8.py
@@ -10,6 +10,11 @@ from lib import aria as arialib
 # Import Standard Python libraries
 import os
 import sys
+import requests
+import urllib3
+session = requests.session()
+session.verify = False
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Import json configuration parameters
 env_json_str = libjson.populate_var_from_json_file("json", "lab_environment.json")
@@ -27,4 +32,17 @@ liblog.write_to_logs(err, logfile_name)
 err = ""
 liblog.write_to_logs(err, logfile_name)
 
-print("YAY!")
+# Local Functions
+def _main_():
+    print("Deploying Aria Suite.")
+    err = "Connecting to vCenter: IP="+env_json_py["vcf"]["management_vcenter"]["ip"]+", UN="+env_json_py["vcf"]["management_vcenter"]["username"]+", "+env_json_py["vcf"]["management_vcenter"]["password"]
+    liblog.write_to_logs(err, logfile_name)
+    vsphere_client = arialib.connect_to_vcenter(env_json_py["vcf"]["management_vcenter"]["ip"], env_json_py["vcf"]["management_vcenter"]["username"], env_json_py["vcf"]["management_vcenter"]["password"], session)
+    err = "Getting VM List."
+    liblog.write_to_logs(err, logfile_name)
+    vm_list = arialib.list_vms_from_vcenter(vsphere_client)
+    vm_count = len(vm_list)
+    err = "VM Count: "+str(vm_count)
+    liblog.write_to_logs(err, logfile_name)
+
+_main_()
--- lib/aria.py
+++ lib/aria.py
@@ -1,12 +1,7 @@
 # vSphere Automation Python SDK Imports
-import requests
-import urllib3
 from vmware.vapi.vsphere.client import create_vsphere_client
-session = requests.session()
-session.verify = False
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# vSphere Python SDK Functions
+# vSphere Automation Python SDK Functions
 def connect_to_vcenter(vcenter_ip, vcenter_username, vcenter_password, session):
     vsphere_client = create_vsphere_client(server=vcenter_ip, username=vcenter_username, password=vcenter_password, session=session)
     return vsphere_client
